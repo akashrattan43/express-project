6c52175f6beeb74f9cb1dcb887222e9427cc682e
akash rattan <akashrattan43@Gmail.com>
2020-11-01 17:07:28 -0800

    env

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, () => 
     console.log(`SERVER STARTED ON PORT ${PORT}`)
-})
+)
