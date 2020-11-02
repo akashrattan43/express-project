45fb23461b674017bef51881d6e1a05849a83b3c
akash rattan <akashrattan43@Gmail.com>
2020-11-01 19:45:04 -0800

    members

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const path = require('path')
-const members = require('./Members.js')
-
 const logger = require('./middleware/logger')
 
 const app = express()
@@ -11,18 +9,10 @@ const app = express()
 //Init logger
 // app.use(logger)
 
-//gets all members
-app.get('/api/members', (req, res) => {
-    res.json(members)
-})
-
-//Get single member
-app.get('/api/members/:id', (req, res) => {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)))
-})
-
 //Set Static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000
 
